ccf22d22e033aa873972740e67a933d673a3052a
al.zach <al.zacharakis@gmail.com>
2025-12-02 23:03:09 +0200

    feat: add mulapp

---
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class MulApp {
+
+    public static void main(String[] args) {
+        System.out.println(mulApp(4, 5));
+
+    }
+    public static int mulApp (int a, int b) {
+        return a * b;
+    }
+}
